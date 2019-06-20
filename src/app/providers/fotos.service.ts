32d2447a3748d98ec8789f059f13d184ea6cf8fb
josemtm <maximilianst@hotmail.com>
2019-06-20 11:12:19 +0200

    agregue la logica para subir las imegenes a firebase en el servicio hay un error que no lo deja subir a la db pero si al storage

--- src/app/providers/fotos.service.ts
+++ src/app/providers/fotos.service.ts
@@ -8,12 +8,41 @@ import { FileItem } from '../models/file-item';
 })
 export class FotosService {
 
-  private CARPETA_IMAGENES = "string";
+  private CARPETA_IMAGENES = "img";
 
   constructor(private db: AngularFirestore) { }
 
   cargarImagenesFirebase(imagenes: FileItem[]){
-    console.log(imagenes)
+    const storageRef = firebase.storage().ref()
+
+    for(const item of imagenes){
+      item.estaSubiendo = true;
+      if (item.progreso >= 100){
+        continue;
+      }
+
+      const uploadTask: firebase.storage.UploadTask=
+                        storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`)
+                        .put(item.archivo)
+
+                        uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
+                          ( snapshot: firebase.storage.UploadTaskSnapshot ) =>
+                                      item.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100,
+                          ( error ) => console.error('Error al subir', error ),
+                           () => {
+            
+                            console.log('Imagen cargada correctamente');
+                             item.url =  uploadTask.snapshot.downloadURL
+                             
+                            item.estaSubiendo = false;
+                            this.guardarImagen({
+                              nombre: item.nombreArchivo,
+                              url: item.url
+                            });
+                          });
+
+    }
+
   }
 
   private guardarImagen(imagen:{nombre:string, url:string}){
